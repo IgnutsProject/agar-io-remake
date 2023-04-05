384ec2c2c3f1f99af6b9c6c6c1f50619ac708a67
Ignut <mister.strays@gmail.com>
2023-04-05 16:15:41 +0300

    Update PlayerCamera.cs

--- Assets/Scripts/Gameplay/Player/PlayerCamera.cs
+++ Assets/Scripts/Gameplay/Player/PlayerCamera.cs
@@ -5,12 +5,49 @@ namespace Gameplay.Player
 {
     public class PlayerCamera : MonoBehaviour
     {
+        [Header("Size properties")] 
+        [SerializeField] private EntityScaler playerScaler;
+        [SerializeField] private AnimationCurve sizeCurve;
+        [SerializeField] private float maxSize = 25f;
+        [SerializeField] private float changeSizeSpeed = 4f;
+        
+        [Header("Movement properties")]
         [SerializeField] private Vector3 offset = new Vector3(0,0,-10);
         [SerializeField] private Transform targetPoint;
+
+        private Camera _camera;
+        private float _defaultSize;
+        private float _targetSize;
+        
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+            _defaultSize = _camera.orthographicSize;
+        }
+
+        private void Start()
+        {
+            playerScaler.OnChangeScale += value =>
+            {
+                _targetSize = _defaultSize + maxSize * sizeCurve.Evaluate(value / playerScaler.MaxValue);
+            };
+        }
 
         private void Update()
         {
             transform.position = targetPoint.position + offset;
+            
+            ChangeSizeToTarget();
+        }
+
+        private void ChangeSizeToTarget()
+        {
+            float newSize = Mathf.MoveTowards(
+                _camera.orthographicSize,
+                _targetSize, 
+                changeSizeSpeed * Time.deltaTime);
+
+            _camera.orthographicSize = newSize;
         }
     }
 }
